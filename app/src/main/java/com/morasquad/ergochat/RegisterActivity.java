a8ee56916cbeb809fea2293214383e5b6137fe37
Hansa <hansatharukarcg3@gmail.com>
2017-12-17 18:24:58 +0530

    after Profile Settings Part completed with picasso T13

--- app/src/main/java/com/morasquad/ergochat/RegisterActivity.java
+++ app/src/main/java/com/morasquad/ergochat/RegisterActivity.java
@@ -20,6 +20,10 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -29,6 +33,8 @@ public class RegisterActivity extends AppCompatActivity {
     private Button mCreateBtn;
 
     private Toolbar mToolBar;
+
+    private DatabaseReference mDatabase;
 
 
     //progras dialog
@@ -85,19 +91,39 @@ public class RegisterActivity extends AppCompatActivity {
         });
     }
 
-    private void register_user(String display_name, String email, String password) {
+    private void register_user(final String display_name, String email, String password) {
         mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
 
                 if (task.isSuccessful()){
 
-                    mRegProgress.dismiss();
+                    FirebaseUser current_user =FirebaseAuth.getInstance().getCurrentUser();
+                    String uid = current_user.getUid();
 
-                    Intent mainIntent = new Intent(RegisterActivity.this,MainActivity.class);
-                    mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT|Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                    startActivity(mainIntent);
-                    finish();
+                    mDatabase =FirebaseDatabase.getInstance().getReference().child("Users").child(uid);
+                    HashMap<String , String> userMap = new HashMap<>();
+                    userMap.put("name",display_name);
+                    userMap.put("status","Hi there i'm using ergo Chat");
+                    userMap.put("image","default");
+                    userMap.put("thumb_image","default");
+
+                    mDatabase.setValue(userMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if(task.isSuccessful()){
+
+                                mRegProgress.dismiss();
+
+                                Intent mainIntent = new Intent(RegisterActivity.this,MainActivity.class);
+                                mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                startActivity(mainIntent);
+                                finish();
+
+                            }
+                        }
+                    });
+
 
                 }else{
 
